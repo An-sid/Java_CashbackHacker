6689e51f8c52dee4b63ee6e3d9fec7cba2be1ce8
An-sid <sidgh@list.ru>
2021-12-14 16:24:03 +0300

    junit4 added

--- src/test/java/service/CashbackHackServiceTest.java
+++ src/test/java/service/CashbackHackServiceTest.java
@@ -1,40 +1,45 @@
 package service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-@Test
 public class CashbackHackServiceTest {
     CashbackHackService cashServ = new CashbackHackService();
 
+    @Test
     public void whenAmountOnBoundary() {
         int expected = 0;
         int actual = cashServ.remain(2000);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
+    @Test
     public void whenAmountIsNull() {
         int expected = 1000;
         int actual = cashServ.remain(0);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
+    @Test
     public void whenAmountIsAvrg() {
         int expected = 500;
         int actual = cashServ.remain(500);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
+    @Test
     public void whenAmountIsUnderBoundary() {
         int expected = 1;
         int actual = cashServ.remain(999);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
+    @Test
     public void whenAmountIsAboveBoundary() {
         int expected = 999;
         int actual = cashServ.remain(1001);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
+
 }
